cbabfe5e1eccb6d328cf80aa88376fcd6292389d
Yuniar Kurniawan <wawancvx46@gmail.com>
2026-04-23 16:08:55 +0700

    capstone 5 - filter

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,11 +5,13 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
+//Misalnya ingin seperti ini
+
+$routes->get('/', 'Home::index', ['filter' => 'auth']); //mengakses rute ini harus login dulu
 
 $routes->get('login', 'AuthController::login');
 $routes->post('login', 'AuthController::login');
 $routes->get('logout', 'AuthController::logout');
 
-$routes->get('/produk', 'ProdukController::index');
-$routes->get('/keranjang', 'TransaksiController::index');
+$routes->get('produk', 'ProdukController::index', ['filter' => 'auth']); //mengakses rute ini harus login dulu
+$routes->get('keranjang', 'TransaksiController::index', ['filter' => 'auth']); //mengakses rute ini harus login dulu
