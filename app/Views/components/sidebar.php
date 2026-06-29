5e3a1a7470fb2da6b7630303ccd5356cc1190257
Yuniar Kurniawan <wawancvx46@gmail.com>
2026-06-29 14:11:42 +0700

    capstone11-webservice-server

--- app/Views/components/sidebar.php
+++ app/Views/components/sidebar.php
@@ -32,6 +32,13 @@
         <?php
         }
         ?>
+        
+        <li class="nav-item">
+            <a class="nav-link <?php echo (uri_string() == 'history') ? "" : "collapsed" ?>" href="history">
+                <i class="bi bi-person"></i>
+                <span>History</span>
+            </a>
+        </li><!-- End History Nav -->
         <!-- munculkan menu profile -->
         <li class="nav-item">
             <a class="nav-link <?php echo (uri_string() == 'profile') ? "" : "collapsed" ?>" href="profile">
